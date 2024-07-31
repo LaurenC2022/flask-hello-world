e19b450cef97f43cd6f613f9c54efc77f9fe2467
Lauren <46947791+LaurenC2022@users.noreply.github.com>
2024-07-31 22:22:59 +0000

    include database

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
+import psycopg2
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World from --your name-- in 3308'
+
+@app.route('/db_test')
+def testing():
+    conn = psycopg2.connect("your_db_url_here")
+    conn.close()
+    return "Database Connection Successfull"
